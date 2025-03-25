734183d601c866ed4ca4ae79fc615bc21971f58d
alicankurus <alicankurus.dev@gmail.com>
2025-03-25 13:57:26 +0100

    data updated

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -18,16 +18,20 @@ public DbSet<Product>  Products =>Set<Product>();
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Product>().HasData(
             new List<Product>{
-                new Product{ ProductId=1 , Name="Iphone14", Description ="telephone",Price =7000,
+                new Product{ ProductId=1 , Name="Apple Watch Series 1", Description ="telephone",Price =7000,
                 IsActive=true, ImageUrl="1.jpg",Stock=4},
-                 new Product{ ProductId=2 , Name="Iphone15", Description ="telephone",Price =5000,
+                 new Product{ ProductId=2 , Name="Apple Watch Series 2", Description ="telephone",Price =5000,
                 IsActive=true, ImageUrl="2.jpg",Stock=9},
-                 new Product{ ProductId=3 , Name="Iphone16", Description ="telephone",Price =2000,
+                 new Product{ ProductId=3 , Name="Apple Watch Series 3", Description ="telephone",Price =2000,
                 IsActive=true, ImageUrl="3.jpg",Stock=2},
-                new Product{ ProductId=4 , Name="Iphone17", Description ="telephone",Price =12000,
+                new Product{ ProductId=4 , Name="Xiaomi Redmi Watch 1", Description ="telephone",Price =8000,
                 IsActive=true, ImageUrl="4.jpg",Stock=2},
-                 new Product{ ProductId=5 , Name="Iphone18", Description ="telephone",Price =12000,
-                IsActive=true, ImageUrl="5.jpg",Stock=2}
+                 new Product{ ProductId=5 , Name="Xiaomi Redmi Watch 2", Description ="telephone",Price =19000,
+                IsActive=true, ImageUrl="5.jpg",Stock=2},
+                new Product{ ProductId=6 , Name="Xiaomi Redmi Watch 3", Description ="telephone",Price =14000,
+                IsActive=true, ImageUrl="6.jpg",Stock=2},
+                new Product{ ProductId=7 , Name="Xiaomi Redmi Watch 4", Description ="telephone",Price =12000,
+                IsActive=true, ImageUrl="7.jpg",Stock=2}
             }
         );
     }
---
+++ API/Migrations/20250325125332_AddNewProducts.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Name",
+                value: "Apple Watch Series 1");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "Name",
+                value: "Apple Watch Series 2");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "Name",
+                value: "Apple Watch Series 3");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                columns: new[] { "Name", "Price" },
+                values: new object[] { "Xiaomi Redmi Watch 1", 8000m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5,
+                columns: new[] { "Name", "Price" },
+                values: new object[] { "Xiaomi Redmi Watch 2", 19000m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 6, "telephone", "6.jpg", true, "Xiaomi Redmi Watch 3", 14000m, 2 },
+                    { 7, "telephone", "7.jpg", true, "Xiaomi Redmi Watch 4", 12000m, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Name",
+                value: "Iphone14");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "Name",
+                value: "Iphone15");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "Name",
+                value: "Iphone16");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                columns: new[] { "Name", "Price" },
+                values: new object[] { "Iphone17", 12000m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5,
+                columns: new[] { "Name", "Price" },
+                values: new object[] { "Iphone18", 12000m });
+        }
+    }
+}
