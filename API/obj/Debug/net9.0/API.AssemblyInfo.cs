2cf1ea72954bab2154d1c32a60eb970bc7c98c16
alicankurus <alicankurus.dev@gmail.com>
2025-03-26 13:13:20 +0100

    navbar links and basket added

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f173758fcda63beb570f71f7d51a5befd14809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf73e31d05a1ba18df32fa6f635fd0c0156e721")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,24 +1,53 @@
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Box, IconButton, Badge, Stack, Button } from "@mui/material";
+import { NavLink } from "react-router";
+import { ShoppingCart } from "@mui/icons-material";
 
 const Header = () => {
-  return (
-    <div> 
+  const links = [
+    { title: "Home", to: "/" },
+    { title: "Catalog", to: "/catalog" },
+    { title: "About", to: "about" },
+    { title: "Contact", to: "/contact" },
+  ];
+  const navStyles = {
+    color: "inherit",
+    textDecoration: "none",
+    "&:hover": {
+      color: "text-primary",
+    },
+    "&.active": {
+      color: "warning.main",
+    },
+  };
 
-<AppBar sx={{mb:4}} position="static" color="primary">
-  <Toolbar>
-    <Container maxWidth="xs">
-    <Typography variant="h6">
-      E-Commerce
-    </Typography>
-    </Container>
-    
-  </Toolbar>
-</AppBar>
-     </div>
-  )
-}
+  return (
+    <div>
+      <AppBar sx={{ mb: 4 }} position="static" color="primary">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography variant="h6">E-commerce</Typography>
+            <Stack direction="row">
+              {links.map((link) => (
+                <Button component={NavLink} sx={navStyles} to={link.to}>
+                  {link.title}
+                </Button>
+              ))}
+            </Stack>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton size="large" edge="start" color="inherit">
+              <Badge badgeContent="2" color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export default Header;
